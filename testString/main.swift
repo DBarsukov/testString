235f7a8ee544103c4412abb3457647de396fd2a3
Dmytro Barsukov <dbarsukov@malkosua.com>
2015-02-02 11:50:29 +0200

    Initial Commit

---
+++ testString/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testString
+//
+//  Created by Dmytro Barsukov on 2/2/15.
+//  Copyright (c) 2015 Dmytro Barsukov. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
